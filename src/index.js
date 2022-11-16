3bdf3d6a0982d0924d86d19344b6b8e93713c820
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-11-16 12:59:32 +0300

    Adding Files to a Preset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
-  <StrictMode>
-  <App/>
-  </StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
